708a90183268dfc2bdf5ef91e67e9f3f28bebe82
threfire <2359716419@qq.com>
2026-02-22 04:06:49 +0800

    完善重力补偿算法，增加了重力补偿的计算函数，并将相关文件移动到User/APP目录下。

---
+++ MDK-ARM/CtrlBoard-H7_WS1812/gravity_comp.d
@@ -0,0 +1,3 @@
+ctrlboard-h7_ws1812\gravity_comp.o: ..\User\APP\Gravity_comp.c
+ctrlboard-h7_ws1812\gravity_comp.o: D:\Keil5\ARM\ARMCC\Bin\..\include\math.h
+ctrlboard-h7_ws1812\gravity_comp.o: ..\User\APP\Gravity_comp.h
---
+++ User/APP/Gravity_comp.h
@@ -0,0 +1,7 @@
+#ifndef GRAVITY_COMP_H
+#define GRAVITY_COMP_H
+
+/* 重力补偿计算函数声明 */
+void gravity_compensation(const float theta[6], float tau[6]);
+
+#endif
